c77dcf4cc8bcb25c487f62f3ca904d7fe5731394
Jpdeke <56802289+Jpdeke@users.noreply.github.com>
2020-05-04 09:19:26 -0300

    respondendo desafio 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -29,13 +29,17 @@ function myFunction2 (arg , x) {
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-var arr1 = [ 'pedro', 1 , true , 0, null];
+var arr1 = [ 'pedro', 1.9 , true , ['joao',20], {b: 2}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-myFunction1(arr1); //(5) ["pedro", 1, true, 0, null]
+console.log( myFunction1(arr1 , 0) ); //(0) "pedro"
+console.log( myFunction1(arr1 , 1) ); //(1) 1.9
+console.log( myFunction1(arr1 , 2) ); //(2) True
+console.log( myFunction1(arr1 , 3) ); //(3) ['joao',20]
+console.log( myFunction1(arr1 , 4) ); //(4) {b: 2}
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -50,56 +54,49 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-function book (parmet) {
-     let books = {
-        livro1:{
-            quantidadePaginas: 0,
-            autor: '',
-            editora: ''                        
+function book ( parmet ) {
+     let allBooks = {
+        'Segredos do Ninja Js':{
+            quantidadePaginas: 480,
+            autor: 'John Resing & Bear Bibeault',
+            editora: 'Novatec'                        
         },
-        livro2: {
-            quantidadePaginas:0 ,
-            autor: '',
-            editora: '' 
+        'Introducao ao Mundo Js': {
+            quantidadePaginas: 220,
+            autor: 'Bruce Lawson & Remy Sharp',
+            editora: 'Alta Books'
         },
-        livro3: {
-            quantidadePaginas:0 ,
-            autor: '',
-            editora: '' 
+        'Aulas Gratuitas do Verdadeiro': {
+            quantidadePaginas: 20,
+            autor: 'Fallen o Vardadeiro',
+            editora: 'Gamers Academy' 
         }          
-    };
-    books = parmet;
-
-
-    return parmet;
-
-
-
-
+    };   
+    return !parmet ? allBooks : allBooks[ parmet ];
 }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log( book() );
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
-
+console.log( 'O livro Aulas Gratuitas do Verdadeiro tem ' + book( 'Aulas Gratuitas do Verdadeiro' ).quantidadePaginas+ " páginas!" );
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log( 'O Autor do Livro Introducao ao Mundo Js é '+book('Introducao ao Mundo Js').autor );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro Segredos do Ninja Js foi publicado pela editora "+book('Segredos do Ninja Js').editora);
+
